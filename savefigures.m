8860e6a339e6400862e532ed829423b99ffa20cc
Rhenan <rhenan.bartels@gmail.com>
2013-06-04 20:00:27 -0300

    Update savefigures.m

--- savefigures.m
+++ savefigures.m
@@ -1,27 +1,54 @@
 options = {};
 N = 1;
-if exist('Pxx','var')
-    options{N} = 'Pxx';
-    N = N + 1;
-end
 if exist('Pxx_AR','var')
-    options{N} = 'Pxx_AR';
-    N = N + 1;
-end
-if exist('RMSSDj','var')
-    options{N} = 'Time Varying';
+    options{N} = 'AR-Periodogram';
+    varc{N} = 'Pxx_AR';
+    N=N+1;
+    varc{N} = 'F_AR';
     N = N + 1;
 end
 if exist('ECG','var')
     options{N} = 'ECG';
+    varc{N} = 'ecg_time';
+    N = N+1;
+    varc{N} = 'ECG';
     N = N + 1;
 end
 if exist('iRR','var')
-    options{N} = 'iRR';
+    options{N} = 'RRi';
+    varc{N} = 'iRR';
+    N = N + 1;
+    varc{N} = 'Time';
     N = N + 1;
 end
+if exist('Pxx','var')
+    options{N} = 'Welch-Periodogram';
+    varc{N} = 'Pxx';
+    N=N+1;
+    varc{N} = 'F';
+    N = N + 1;
+end
+
+if exist('RMSSDj','var')
+    options{N} = 'Time Varying';
+    varc{N} = 'RMSSDj';
+    N=N+1;
+    varc{N} = 'pNN50j';
+    N=N+1;
+    varc{N} = 'SDNNj';
+    N=N+1;
+    varc{N} = 'averagej';
+    N=N+1;
+    varc{N} = 'Index';
+    N = N + 1;
+end
+
+
 if exist('STFT','var')
     options{N} = 'STFT';
+    varc{N} = 'Pft';
+    N=N+1;
+    varc{N} = 'Fft';
     N = N + 1;
 end
 if exist('STAR','var')
@@ -29,7 +56,9 @@ if exist('STAR','var')
     N = N + 1;
 end
 
-if N > 1
+options = sort(options);
+
+if N > 2
     dpilist = ['150';'300';'600'];
     colorlist = {'Black';'White';'Red';'Yellow';'Cyan';'Green'};
     plotlist = {'Line';'Dot';'Dashed-Line';'Line-Dot';'Dot-Line';'Hist'};
@@ -37,7 +66,7 @@ if N > 1
         'number','off','position',[300,300,300,270],'resize','off',...
         'color',[0.839,0.91,0.851],'MenuBar','none');
     lsf = uicontrol('Style','listbox','Position',[10,40,170,200],'String',...
-        'options');
+        options);
     p1sv = uicontrol('Style','pop','String',plotlist,'position',...
         [200,190,70,40]);
     p2sv = uicontrol('Style','pop','String',colorlist,'position',...
@@ -45,9 +74,28 @@ if N > 1
     p3sv = uicontrol('Style','pop','String',dpilist,'position',...
         [200,130,70,40]);
     bsf1 = uicontrol('Position',[10,15,120,20],'String', 'Preview',...
-        'CallBack','tempsf = figure();plot(randn(1,100))');
-    %Pegar o indice que foi selecionado na listbox plotar com visualize off
-    %e salvar com saveas
+        'CallBack','set(fsf,''Visible'',''off'')');
+        
+        % TODO: solve problem of sorted index array.
+        
+     switch get(lsf,'Value')
+        case 1
+            fsf = figure();
+            plot(F,Pxx);
+            set(fsf,'Visible','off')
+        case 2
+            fsf = plot(F_AR,Pxx_AR);
+            set(fsf,'Visible','off')
+        case 3
+            fsf = plot(Index,RMSSDj);
+            set(fsf,'Visible','off')
+        case 4
+            fsf = plot(ecg_time, ECG);
+            set(fsf,'Visible','off')
+        case 5
+            fsf = plot(Time, iRR);
+            set(fsf,'Visible','off')
+    end
     bsf2 = uicontrol('String','Save','Callback',['[filesf, pathsf] =',...
         'uiputfile({''*.png'';''*.tif'';''*.pdf'';''*.*''},''Save Figure Dialog'',''Figure_1'');',...
         'saveas(fsf,[pathsf,filesf]);'],'position',[132,15,120,20]);
