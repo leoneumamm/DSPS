28ca4aba498f502160de3e75c399151153a83485
Rhenan <rhenan.bartels@gmail.com>
2013-06-06 20:04:59 -0300

    Update timefrequency.m

--- timefrequency.m
+++ timefrequency.m
@@ -13,66 +13,90 @@ if isempty(Fs)
     num_lines = 1;
     def = {'4','512','256'};
     answer = inputdlg(prompt,dlg_title,num_lines,def);
-    Fs = str2double(answer{1});
-    segment = str2double(answer{2});
-    overlap = str2double(answer{3});
+    if isempty(cellfun(@isempty,answer))
+        Pft=0;
+        Fft=0;
+        LF_STFT=0;
+        HF_STFT=0;
+        window_stft=0;
+        segment=0;
+        overlap=0;
+        return
+    else
+        Fs = str2double(answer{1});
+        segment = str2double(answer{2});
+        overlap = str2double(answer{3});
+    end
 else
     prompt = {'Enter Segment Size:','Enter Overlap Size:'};
     dlg_title = 'STFT Parameters';
     num_lines = 1;
     def = {'512','256'};
     answer = inputdlg(prompt,dlg_title,num_lines,def);
-    segment = str2double(answer{1});
-    overlap = str2double(answer{2});
+    if isempty(cellfun(@isempty,answer))
+        Pft=0;
+        Fft=0;
+        LF_STFT=0;
+        HF_STFT=0;
+        window_stft=0;
+        segment=0;
+        overlap=0;
+        return
+    else
+        segment = str2double(answer{1});
+        overlap = str2double(answer{2});
+    end
 end
 if overlap >  segment,
     er = errordlg('Overlap Must be Smaller Than the Segment! Try Again','Range Error');
     uiwait(er)
     [Pft,Fft,LF_STFT,HF_STFT] = timefrequency(iRR,Time,Fs);
 else
-[window, control] = window_select();
-
-step = segment - overlap;
-L = length(iRR);                                                                             % Elsenbruch et al., 20000
-iter = floor((L-segment)/step) + 1;
-start = 1;
-stop = segment;
-for i=1:iter,
-    irr_temp = iRR(start:stop) - mean(iRR(start:stop));
-    if window == 1
-        [Pft(:,i),Fft]=periodogram(irr_temp,hanning(size(irr_temp,2)),[],Fs);
-        window_stft = 'Hanning';
-    elseif window == 2
-        [Pft(:,i),Fft]=periodogram(irr_temp,triang(size(irr_temp,2)),[],Fs);
-        window_stft = 'Triangular';
-    elseif window == 3
-        [Pft(:,i),Fft]=periodogram(irr_temp,blackman(size(irr_temp,2)),[],Fs);
-        window_stft = 'Blackman';
-    elseif window == 4
-        [Pft(:,i),Fft]=periodogram(irr_temp,hamming(size(irr_temp,2)),[],Fs);
-        window_stft = 'Hamming';
-    elseif window == 5
-        [Pft(:,i),Fft]=periodogram(irr_temp,kaiser(size(irr_temp,2)),[],Fs);
-        window_stft = 'Kaiser';
-    elseif window == 6
-        [Pft(:,i),Fft]=periodogram(irr_temp,gausswin(size(irr_temp,2)),[],Fs);
-        window_stft = 'Gaussian';
-    else
-        [Pft(:,i),Fft]=periodogram(irr_temp,rectwin(size(irr_temp,2)),[],Fs);
-        window_stft = 'Rectangular';
+    [window, control] = window_select();
+    
+    step = segment - overlap;
+    L = length(iRR);                                                                             % Elsenbruch et al., 20000
+    iter = floor((L-segment)/step) + 1;
+    STFTt = 1;
+    stop = segment;
+    for i=1:iter,
+        irr_temp = iRR(STFTt:stop) - mean(iRR(STFTt:stop));
+        if window == 1
+            [Pft(:,i),Fft]=periodogram(irr_temp,hanning(size(irr_temp,2)),[],Fs);
+            window_stft = 'Hanning';
+        elseif window == 2
+            [Pft(:,i),Fft]=periodogram(irr_temp,triang(size(irr_temp,2)),[],Fs);
+            window_stft = 'Triangular';
+        elseif window == 3
+            [Pft(:,i),Fft]=periodogram(irr_temp,blackman(size(irr_temp,2)),[],Fs);
+            window_stft = 'Blackman';
+        elseif window == 4
+            [Pft(:,i),Fft]=periodogram(irr_temp,hamming(size(irr_temp,2)),[],Fs);
+            window_stft = 'Hamming';
+        elseif window == 5
+            [Pft(:,i),Fft]=periodogram(irr_temp,kaiser(size(irr_temp,2)),[],Fs);
+            window_stft = 'Kaiser';
+        elseif window == 6
+            [Pft(:,i),Fft]=periodogram(irr_temp,gausswin(size(irr_temp,2)),[],Fs);
+            window_stft = 'Gaussian';
+        else
+            [Pft(:,i),Fft]=periodogram(irr_temp,rectwin(size(irr_temp,2)),[],Fs);
+            window_stft = 'Rectangular';
+        end
+        STFTt = STFTt + step;
+        stop = stop + step;
     end
-    start = start + step;
-    stop = stop + step;
+    [HF_STFT,LF_STFT] = integral(Pft,Fft,iter);
+    Pot = 10*log10(Pft);
+    Pot(Pot < 0) = 0;
+    axes(ax)
+    imagesc(Time,Fft,Pot);
+    shading interp
+    ylim([0,1])
+    set(gca,'YDir','Normal')
+    title('Time Frequency Analysis - STFT')
+    xlabel('Time (s)')
+    ylabel('Frequency (Hz)')
 end
-[HF_STFT,LF_STFT] = integral(Pft,Fft,iter);
-Pot = 10*log10(Pft);
-Pot(Pot < 0) = 0;
-axes(ax)
-imagesc(Time,Fft,Pot);
-shading interp
-ylim([0,1])
-set(gca,'YDir','Normal')
-title('Time Frequency Analysis - STFT')
-xlabel('Time (s)')
-ylabel('Frequency (Hz)')
-end
+
+%TODO: Zero-Padding.
