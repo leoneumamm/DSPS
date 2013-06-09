8de9fe75136aba0527f1cf56af72cc2393e9c4ae
leoneumamm <leoneumamm@gmail.com>
2013-06-09 19:52:51 -0300

    Update timedomain.m
    troquei o rms...minha versão do Matlab não tem essa função

--- timedomain.m
+++ timedomain.m
@@ -1,6 +1,6 @@
 function [RMSSD,pNN50,SDNN,SD1,SD2] = timedomain(iRR)
 
-RMSSD = rms(diff(iRR));
+RMSSD = sqrt(sum(diff(iRR).*(diff(iRR)))/size(diff(iRR),1));
 pNN50_control = gradient(iRR) >= 50;
 pNN50 = (sum(pNN50_control)/length(iRR))*100;
 SDNN = std(iRR);
