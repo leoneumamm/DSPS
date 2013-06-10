e02ae3172ba512bfdf24f72747bd81a61ba78733
Rhenan <rhenan.bartels@gmail.com>
2013-06-10 17:13:19 -0300

    Update timefrequencyar.m

--- timefrequencyar.m
+++ timefrequencyar.m
@@ -1,4 +1,5 @@
-function [Par,Far,LF_STAR,HF_STAR,iRR,Time,Fs,window_star,segment,overlap] = timefrequencyar(iRR,Time,Fs,ax)
+function [Par,Far,LF_STAR,HF_STAR,iRR,Time,time_star,Fs,window_star,segment,...
+    overlap] = timefrequencyar(iRR,Time,Fs,ax)
 time_test1 = Time(3) - Time(2);
 time_test2 = Time(2) - Time(1);
 
@@ -9,10 +10,10 @@ if abs(time_test1 - time_test2) > 10e-3;
 end
 
 if isempty(Fs)
-    prompt = {'Sampling Frequency','Model Order','Enter Segment Size:','Enter Overlap Size:'};
+    prompt = {'Sampling Frequency','Model Order','Enter Segment Size:','Enter Overlap Size:','Zero-Padding'};
     dlg_title = 'STAR Parameters';
     num_lines = 1;
-    def = {'4','14','512','256'};
+    def = {'4','14','512','256','0'};
     answer = inputdlg(prompt,dlg_title,num_lines,def);
     if isempty(cellfun(@isempty,answer))
         Par=0;
@@ -28,13 +29,14 @@ if isempty(Fs)
         order = str2double(answer{2});
         segment = str2double(answer{3});
         overlap = str2double(answer{4});
+        zPc = str2double(answer{5});
     end
 else
     
-    prompt = {'Model Order','Enter Segment Size:','Enter Overlap Size:'};
+    prompt = {'Model Order','Enter Segment Size:','Enter Overlap Size:','Zero-Padding'};
     dlg_title = 'STAR Parameters';
     num_lines = 1;
-    def = {'14','512','256'};
+    def = {'14','512','256','0'};
     answer = inputdlg(prompt,dlg_title,num_lines,def);
     if isempty(cellfun(@isempty,answer))
         Par=0;
@@ -49,16 +51,28 @@ else
         order = str2double(answer{1});
         segment = str2double(answer{2});
         overlap = str2double(answer{3});
+        zPc = str2double(answer{4});
     end
     
 end
 if overlap >  segment,
     er = errordlg('Overlap Must be Smaller Than the Segment! Try Again','Range Error');
     uiwait(er)
-    [Pft,Fft,LF_STFT,HF_STFT] = timefrequencyar(iRR,Time,Fs,ax);
+    [Par,Far,LF_STAR,HF_STAR,iRR,Time,time_star,Fs,window_star,segment,...
+    overlap] = timefrequencyar(iRR,Time,Fs,ax);
 else
     [window, control] = window_select();
-    
+    if zPc== 0
+        zP = [];
+    else
+        zP = zPc;
+    end
+    if zP ~= fix(zP)
+        er = errordlg('Zero-Padding length must be a integer! Try Again','Zero-Padding Error');
+        uiwait(er)
+        [Par,Far,LF_STAR,HF_STAR,iRR,Time,time_star,Fs,window_star,segment,...
+    overlap] = timefrequencyar(iRR,Time,Fs,ax);
+    end
     step = segment - overlap;
     L = length(iRR);                                                                             % Elsenbruch et al., 2000
     iter = floor((L-segment)/step) + 1;
@@ -68,11 +82,11 @@ else
         irr_temp = iRR(start:stop) - mean(iRR(start:stop));
         if window == 1
             irr_temp = irr_temp.*hanning(length(irr_temp))';
-            [Par(:,i),Far] = pburg(irr_temp,order,[],Fs);
+            [Par(:,i),Far] = pburg(irr_temp,order,zP,Fs);
             window_stft = 'Hanning';
         elseif window == 2
             irr_temp = irr_temp.*triang(length(irr_temp))';
-            [Par(:,i),Far] = pburg(irr_temp,order,[],Fs);
+            [Par(:,i),Far] = pburg(irr_temp,order,zP,Fs);
             window_stft = 'Triangular';
         elseif window == 3
             irr_temp = irr_temp.*blackman(length(irr_temp))';
@@ -80,19 +94,19 @@ else
             window_stft = 'Blackman';
         elseif window == 4
             irr_temp = irr_temp.*hamming(length(irr_temp))';
-            [Par(:,i),Far] = pburg(irr_temp,order,[],Fs);
+            [Par(:,i),Far] = pburg(irr_temp,order,zP,Fs);
             window_stft = 'Hamming';
         elseif window == 5
             irr_temp = irr_temp.*kaiser(length(irr_temp))';
-            [Par(:,i),Far] = pburg(irr_temp,order,[],Fs);
+            [Par(:,i),Far] = pburg(irr_temp,order,zP,Fs);
             window_stft = 'Kaiser';
         elseif window == 6
             irr_temp = irr_temp.*gausswin(length(irr_temp))';
-            [Par(:,i),Far] = pburg(irr_temp,order,[],Fs);
+            [Par(:,i),Far] = pburg(irr_temp,order,zP,Fs);
             window_stft = 'Gaussian';
         else
             irr_temp = irr_temp.*rectwin(size(irr_temp))';
-            [Par(:,i),Far] = pburg(irr_temp,order,[],Fs);
+            [Par(:,i),Far] = pburg(irr_temp,order,zP,Fs);
             window_stft = 'Rectangular';
         end
         start = start + step;
@@ -101,6 +115,7 @@ else
     [HF_STAR,LF_STAR] = integral(Par,Far,iter);
     Pot = 10*log10(Par);
     Pot(Pot < 0) = 0;
+    time_star = linspace(0,Time(end),iter);
     axes(ax)
     imagesc(Time,Far,Pot);
     shading interp
@@ -111,4 +126,3 @@ else
     ylabel('Frequency (Hz)')
     window_star = 'Verificar';
 end
-%TODO: Zero-Padding.
